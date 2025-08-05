c9441e037499dc7b1c907a854261051c9f4913ad
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-08-05 18:11:44 +0300

    New name

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import './navbar.css';
 
 function Navbar() {
 	function githubRepoLocation() {
-		window.location.href = 'https://github.com/sh4man4ik/FilmsTok';
+		window.location.href = 'https://github.com/sh4man4ik/MoviesTok';
 	}
 
 	return (
 		<>
 			<div className="navbar">
-				<h1 className="title display-6">FilmsTok</h1>
+				<h1 className="title display-6">MoviesTok</h1>
 				<h1 className="github-link-text display-6" onClick={githubRepoLocation}>
 					GitHub
 				</h1>
